10d96028cafc80d620d3eeae092ed167d4770d7d
foreseaz <foreseazhang@gmail.com>
2019-01-14 15:38:56 +0800

    Amend test

--- src/__tests__/units/keygen.spec.ts
+++ src/__tests__/units/keygen.spec.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-expression-statement
 import test from 'ava'
-import { createPrivateKey } from './keygen'
+import { createPrivateKey } from '../../lib/keygen'
 
 test('create privateKey', async t => {
   let privateKey = createPrivateKey()
